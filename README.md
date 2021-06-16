dcbb332069b814155c25d581f927f51d9aa53a6b
Luís Henrique <luishenri2002@hotmail.com>
2021-06-15 21:13:21 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Calculadora
+Calculadora - FATEC Americana - ADS
+# Aluno
+Luís Henrique da Silva de Almeida
+RA: 2011040
---
+++ calculadora/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:calculadora_flutter/pages/calculator_page.dart';
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return new SplashScreen(
+        seconds: 4,
+        navigateAfterSeconds: new AfterSplash(),
+        title: new Text(
+          'Calculadora',
+          style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0, color: Colors.white),
+        ),
+        image: new Image.asset('assets/splashscreen/calculator_icon.png'),
+        backgroundColor: Colors.deepRed,
+        styleTextUnderTheLoader: new TextStyle(),
+        photoSize: 100.0,
+        onClick: () => print("Flutter Egypt"),
+        loaderColor: Colors.white);
+  }
+}
+
+class AfterSplash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(visualDensity: VisualDensity.adaptivePlatformDensity, primarySwatch: Colors.deepOrange),
+      home: CalculatorPage(),
+    );
+  }
+}
